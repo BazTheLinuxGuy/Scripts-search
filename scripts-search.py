6a540f2a63bb8894ff70be557ae464207a7cc119
Bryan Zimmer <bazthelinuxguy@gmail.com>
2020-09-21 07:28:04 -0500

    Outputs .CSV files - for spreadsheets.
    This program now puts out data in .csv format, with titles on the first line. So far it has imported well into LibreOffice and MS-Excel (caution: this is a Linux/Unix program). There arew four fields (columns): 1) the name of the program 2) A brief "whatis" summary of the program, when available, 3) The type of the program (e.g. Bourne Shell script, Python Script, Ruby, Perl, Lua, REXX, makefile ro symlink),, 4) Size, in bytes, of the program.

--- scripts-search.py
+++ scripts-search.py
@@ -19,13 +19,13 @@ r'''scripts-search.py, much like its predecessors,
                                   NOTE: Must be fully qualified
                 -v / --verbose: [increase program verbosity, up to 3 -v's
                                  can be used.]
-                -r / --generate-report: [create a report showing statistics of scripts]
+                -r / --generate-report: [create a report showing \
+                                         statistics of scripts]
                 -p [parse the list of scripts into categories:
                    Perl Scripts, Python, sh scripots, Ruby, Lua,
                    whatever...] (NOT YET IMPLEMENTED)
                 -s [sort scripts array by program name, program type,\
                     or program size.]
-
 '''
 
 import regex
@@ -40,17 +40,11 @@ import os
 
 __author__ = 'Bryan A. Zimmer'
 __date_of_this_notation__ = 'September 1, 2020 9:50 AM'
-__updated__ = 'September 17, 2020 6:00 AM (Thursday)'
+__updated__ = 'September 18, 2020 4:25 PM (Friday))'
 __program_name__ = 'get_scripts.py'
-## My latest idea is to output the reports as CSV...that way
-## they could be incorporated into Excel or other programs.
 
-HYPHEN = '-'
-
-#global args
 global fileinfo
 
-fileinfo=('fileinfo','progname','progtype','size')
 
 # For backward compatibility, a stat_result instance is also accessible
 # as a tuple of at least 10 integers giving the most important (and
@@ -61,37 +55,18 @@ fileinfo=('fileinfo','progname','progtype','size')
 # (from 'stat' documentation).
 
 
-fileinfo = namedtuple('fileinfo', 'progname, progtype, size')
+fileinfo = namedtuple('fileinfo', 'progname, whatis, progtype, size')
 
-
-def startup_housekeeping():
-    r'''move this to startup housekeeping()        
-            we need the name of the output directory from the 
-            argparse routine then something like this'''
- #   global args
-
-    if args.outputdir.endswith(os.sep):
-        args.outputdir = args.outputdir[0:-1]
-    replace = HYPHEN
-    sfilename = 'scripts-in'
-    inputdir = regex.sub(os.sep, HYPHEN, args.inputdir)
-    sfile = sfilename + inputdir + '.csv'
-    
-    if args.verbose > 0:
-        print('sfile =', sfile)
-        sleep(2)
-    return sfile
 
 
 def parse_args():
-#    global args
     r'''The purpose of args here is to get the input directory/ies
     and the output directory where the results of this program will go.
     There is the matter of naming the output files, but that is better
     done elsewhere. Here, we will stick to the command line args.'''
     program = os.path.basename(sys.argv[0])
     parser = argparse.ArgumentParser(description='Parse command line \
-for the program.', prog=program)
+                                     for the program.', prog=program)
     parser.add_argument('-i', '--inputdir',
                         help='Input directory, a "bin" dir.',
                         required=True)
@@ -101,7 +76,7 @@ for the program.', prog=program)
     parser.add_argument('-v', '--verbose', action='count',
                         default=0, required=False,
                         help='Increase program verbosity')
-    parser.add_argument('-r', '--generate=report',
+    parser.add_argument('-r', '--report',
                         action='store_true', required=False, default=False,
                         help='Statistics on the output we produce.')
     parser.add_argument('-s','--sortby', action = 'store',
@@ -114,6 +89,24 @@ for the program.', prog=program)
     args = parser.parse_args()
     return args
 
+def startup_housekeeping():
+    r'''move this to startup housekeeping()        
+            we need the name of the output directory from the 
+            argparse routine then something like this'''
+
+    if args.outputdir.endswith(os.sep):
+        args.outputdir = args.outputdir[0:-1]
+    replace = '-'
+    sfilename = 'scripts-in'
+    inputdir = regex.sub(os.sep, '-', args.inputdir)
+    sfile = sfilename + inputdir + '.csv'
+    
+    if args.verbose > 0:
+        print('sfile =', sfile)
+        sleep(2)
+    return sfile
+
+
 
 class Script:
     r''' This class creates a 3-tuple for `scripts'
@@ -122,16 +115,18 @@ class Script:
 
     pg_type = 'single executable script'
 
-    def __init__(self, progname, progtype, size):
+    def __init__(self, progname, whatis, progtype, size):
         self.scripts = []
         self.progname = progname
+        self.whatis = whatis
         self.progtype = progtype
         self.size = size
-        self.finfo = fileinfo(self.progname, self.progtype, self.size)
+        self.finfo = fileinfo(self.progname, self.whatis,
+                              self.progtype, self.size)
 
     def process_progtype(self):
         r''' This is the method that refines disparate data
-             into a refined "script" a 3-tuple.'''
+             into a refined "script" a named 4-tuple.'''
 
         line = self.finfo.progtype.rstrip()
         n = line.index(':')
@@ -146,39 +141,21 @@ class Script:
         self.progtype = line
 
         if self.finfo.progtype.endswith(','):
-            self.findo.progtype = self.finfo.progtype[0:-1]
-        self.finfo = fileinfo(self.progname, self.progtype, self.size)
+            self.finfo.progtype = self.finfo.progtype[0:-1]
+        self.finfo = fileinfo(self.progname, self.whatis, self.progtype, self.size)
         return self.finfo
 
 ### -------- End of class Script -------- ###
 
 def straighten_up(datum):
 
-    # NOTE: I'm pretty sure that none of the
-    # fields contains more than one comma.
-    # If I'm wrong I'm going to have to resort
-    # to regex.
 
     anathema = ','
     replacement = ' -'
     datum = regex.sub(anathema, replacement, datum)
     return datum
 
-
-    
-#    try:
-#        x = datum.index(',')
-#    except ValueError:
-#        return datum
-#    else:
-#        datum1 = datum[0:x]
-#        datum2 = datum[x+1:]
-#        datum = datum1 + ' -' + datum2
-#        return datum
-    
-
 class Scripts:
-#    global args
 
     pg_type = 'executable scripts'
 
@@ -195,14 +172,10 @@ class Scripts:
     
 
     def writefile(self, sfile):
-        # Though this output is not meant for the end user,
-        # We need to sort by one of the three fields in the
-        # named tuples,
-        
         self.sfile = sfile
-# `sort' or `sorted'?
+#       `sort' or `sorted'?
         self.newlist = self.scripts # sorted alphabetically
-# the user gets to choose what field to sort on.
+#       the user gets to choose what field to sort on.
         if args.sortby == 'type':
             self.newlist = sorted(self.scripts, key=lambda finfo:\
                                   finfo.progtype)
@@ -212,31 +185,28 @@ class Scripts:
         else: # it has to be 'alpha', which it already is sorted to.
             pass
             
-# or:    self.scripts.sort(key=lambda script: key=script.progtype)
-# the third option, alphabetical (ascending)
-# is there by default
-        this = os.getcwd()
-#        print(f'We are now at {this}')
-#        sleep(2)
-#        print('We are writing files at {}.'.format(args.outputdir))
         os.chdir(args.outputdir)
         with open(self.sfile, 'w') as f:
-            s1='Prog Name'
-            s2='Prog Type'
-            s3='Size'
-            s=s1 + ', ' + s2 + ', ' + s3 + '\n'
+            s1='Program Name'
+            s2='"Whatis"'
+            s3='Program Type'
+            s4='Size in bytes'
+            s=s1 + ',' + s2 + ',' + s3 + ',' + s4 + '\n'
             f.write(s)
             for onescript in self.newlist:
                 s1 = onescript.progname
                 s1 = s1.rstrip()
-#                s1 = straighten_up(s1)
-                s2 = onescript.progtype
+                s1 = straighten_up(s1)
+                s2 = onescript.whatis
                 s2 = s2.rstrip()
-                s2 = straighten_up(s2) # change commas to hyphens
-                s3 = str(onescript.size)
+                s2 = straighten_up(s2)
+                s3 = onescript.progtype
                 s3 = s3.rstrip()
-#                s3 = straighten_up(self.s3)
-                s = s1 + ',' + s2 + ',' + s3 + '\n'
+                s3 = straighten_up(s3) # change commas to hyphens
+                s4 = str(onescript.size)
+                s4 = s4.rstrip()
+                s4 = straighten_up(s4)
+                s = s1 + ',' + s2 + ',' + s3  + ', ' + s4 +'\n'
                 f.write(s)
 
         print('\n==> ** Processing complete, {} scripts found. **'.\
@@ -247,17 +217,16 @@ class Scripts:
 ### ------- End of Class Scripts ---------- ###
 
 
-def process_program_list(program_list, scripts):
-#    global args
+def process_programs(programs, scripts):
 
     statinfo = namedtuple('statinfo', 'st_mode, st_ino, st_dev, '
                           'st_nlink, st_uid, st_gid, st_size, st_atime,'
                           ' st_mtime, st_ctime')
 
-    program_list.sort(key=str.lower)
-    # program_list is now in alphabetical order.
+    programs.sort(key=str.lower)
+    # programs is now in alphabetical order.
 
-    for program in program_list:
+    for program in programs:
         try:
             program = program.rstrip()
             if args.verbose == 1:
@@ -275,13 +244,30 @@ def process_program_list(program_list, scripts):
             line = line.rstrip()
             progtype = line
             p.close()
+
+            command = 'whatis ' + program  # + ' > /dev/null'
+            p = os.popen(command)
+            line = p.readline()
+            line = line.rstrip()
+            whatis = line
+            p.close()
+            
+            try:
+                x = whatis.index('- ')
+            except ValueError:
+                pass
+            else:
+                x += 2
+                whatis = whatis[x:]
+                
             if (('compiled' in progtype) or ('ELF' in progtype)):
                 continue
             if (('ASCII' in progtype) or ('script' in progtype) or
                     ('text' in progtype)):
-                script = Script(program, progtype, size)
+                script = Script(program, whatis, progtype, size)
                 onescript = script.process_progtype()
                 scripts.add(onescript)
+
 
         except FileNotFoundError:
             continue
@@ -290,15 +276,14 @@ def process_program_list(program_list, scripts):
 
 
 def main():
-#    global args
 
     sfile = startup_housekeeping()  # sfile names the file that will
                                     # be written in the output directory.
     os.chdir(args.inputdir)
 
-    program_list = []
-    program_list = os.listdir(args.inputdir)
-    numprograms = len(program_list)
+    programs = []
+    programs = os.listdir(args.inputdir)
+    numprograms = len(programs)
 
     if args.verbose > 0:
         print('\n==> There are {} programs in {}.'.
@@ -306,12 +291,13 @@ def main():
         sleep(2)
     if args.verbose > 0:
         print('==> Please be patient. '
+
               'Processing {} programs takes time.'.
               format(numprograms), end='\n\n')
         sleep(2)
     scripts = Scripts()  # Starts out empty
 
-    script_list=process_program_list(program_list, scripts)
+    script_list=process_programs(programs, scripts)
     # the function call populates "scripts" ONLY with
     # "scripts" - Python, Perl, Ruby, php, Bourne Shell, etc.
     # compiled ELF programs are discarded, there are different
@@ -323,12 +309,12 @@ def main():
         sleep(2)
 
     scripts.writefile(sfile)
+    if args.report:
+        report_module
     return scripts.length()
 
 
 if __name__ == '__main__':
-    # call to parse args ##### (or, probably, NOT some configfile)
-#    global args
     args = parse_args()
     here = os.getcwd()
     rc = main()
